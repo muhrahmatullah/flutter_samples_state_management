826e40ba378f9505eb3e321e95673d4cb442ea64
Muh. Rahmatullah <muhammadrahmatullah1@gmail.com>
2020-09-03 00:10:17 +0800

    add item detail screen

--- lib/ui/item_view.dart
+++ lib/ui/item_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:state_management_sample/model/team.dart';
 
+import 'detail_screen.dart';
+
 class NbaItemView extends StatelessWidget {
 
   const NbaItemView({Key key, this.data}) : super(key: key);
@@ -13,6 +15,7 @@ class NbaItemView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: ListTile(
+        onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => NbaTeamDetailScreen(data: data,))),
         title: Text(data.fullName, style: Theme.of(context).textTheme.headline6,),
         subtitle: Text(data.city),
         trailing: Text(data.conference),
